e1130b52f5d51a15860deddfb5c7cd985624e88e
Lauro Brizidio <lauro.brizidio@itbam.org.br>
2021-07-12 14:07:01 -0400

    feat: onBackpressed corrected

--- app/src/main/java/com/brizidiolauro/cleanexample/MainActivity.kt
+++ app/src/main/java/com/brizidiolauro/cleanexample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.brizidiolauro.cleanexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onBackPressed() {
+        findNavController(R.id.nav_host_fragment).popBackStack()
     }
 }
